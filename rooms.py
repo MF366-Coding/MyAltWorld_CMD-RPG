52fb165ef834ce8478af4afdeeb51693948437cf
MF366 <real_mf366@yahoo.com>
2023-05-19 19:37:36 +0100

    rooms and idk
    LET'S GET "ROOM"Y! lmfao wtf, mf366?
    
    done FOR THE DAY
    YES THE FREAKING MEME IS HERE AHAHAWDH111!!!1!
    
    this was supposed to be a productive place, man...

--- rooms.py
+++ rooms.py
@@ -8,20 +8,26 @@ from clsCmd import clear
 class House_1:
     # attributes for your house
     ur_bedroom = Room("""
-                        OH MY GOD!!
-                        How could this happen?
-                        
-                        You're in your bedroom!!!!
-                        It looks messy by the way...
-                        """)
+					OH MY GOD!!
+					How could this happen?
+
+					You're in your bedroom!!!!
+					It looks messy by the way...
+
+					There is only one exit to south.
+					Unless you could jump from the window...
+					I mean it's just a one floor house so...
+					""")
     corridor_2 = Room("""
-                      How interesting!
-                      
-                      By leaving your
-                      """)
+					How interesting!
+
+					By leaving your room, you reach the main corridor.
+
+					There is nothing to the right.
+					Why would it?
+					""")
 
 
 current_room = None
 Room.can_yell = True
 Room.can_brush = False
-	
